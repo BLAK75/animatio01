65f3e761edc2b931ebf28f3f6ce39f932305b357
DaviProgramador <135262443+BLAK75@users.noreply.github.com>
2024-08-14 15:35:41 -0300

    subir meu site

---
+++ scrits.js
@@ -0,0 +1,27 @@
+const list = document.querySelectorAll('.item')
+const next = document.getElementById('next')
+const prev = document.getElementById('prev')
+
+
+
+const count = list.length 
+let active = 0
+
+
+next.onclick = () => {
+    const activeold = document.querySelector('.active')
+    activeold.classList.remove('active')
+
+    active = active >= count -1 ? 0 : active + 1
+    list[active].classList.add('active')
+   
+}
+
+prev.onclick = () => {
+    const activeold = document.querySelector('.active')
+    activeold.classList.remove('active')
+
+    active = active <= 0 ? count -1 : active - 1
+    list[active].classList.add('active')
+    
+}
